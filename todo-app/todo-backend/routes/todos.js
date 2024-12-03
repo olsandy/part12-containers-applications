c912e47b16f9981aeb2158a9fd254ecafcb17298
olsandy <anders.olsson@aalto.fi>
2024-12-03 21:47:34 +0200

    12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,49 +1,60 @@
-const express = require('express');
+const express = require('express')
+const ObjectId = require('mongoose').Types.ObjectId
 const { Todo } = require('../mongo')
-const router = express.Router();
+const router = express.Router()
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
   const todos = await Todo.find({})
-  res.send(todos);
-});
+  res.send(todos)
+})
 
 /* POST todo to listing. */
 router.post('/', async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
+    done: false,
   })
-  res.send(todo);
-});
+  res.send(todo)
+})
 
-const singleRouter = express.Router();
+const singleRouter = express.Router()
 
 const findByIdMiddleware = async (req, res, next) => {
   const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
+  if (ObjectId.isValid(id)) {
+    req.todo = await Todo.findById(id)
+    if (!req.todo) return res.sendStatus(404)
+  } else {
+    return res.sendStatus(404)
+  }
 
   next()
 }
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
-  res.sendStatus(200);
-});
+  await req.todo.delete()
+  res.sendStatus(200)
+})
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
+  res.send(req.todo)
+})
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
+  await Todo.updateOne(
+    { _id: req.todo.id },
+    {
+      text: req.body.text,
+      done: req.body.done || false,
+    }
+  )
+  res.sendStatus(200) // Implement this
+})
 
 router.use('/:id', findByIdMiddleware, singleRouter)
 
-
-module.exports = router;
+module.exports = router
